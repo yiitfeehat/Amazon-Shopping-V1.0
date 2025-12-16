9ff82c5c42666804aea2ce3e45ed46745e5ae44e
yiitfeehat <yiitferhat@gmail.com>
2025-12-16 22:05:21 +0300

    Refactor checkout and cart functionality: update checkout page structure, enhance cart item rendering, and initialize cart with sample data

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export let cart = [];
+export let cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 3
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2
+
+}];
 export function addToCart(productId, quantity) {
     let matchingItem;
     cart.forEach((cartItem) => {
