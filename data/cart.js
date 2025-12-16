7c4b279da2056e738d2a8c27ae5b0cd5cfce0c7b
yiitfeehat <yiitferhat@gmail.com>
2025-12-16 23:11:41 +0300

    Refactor cart initialization: load cart from localStorage and maintain sample data fallback

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 3
-}, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-}];
+if (!cart) {
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 3
+    }, {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2
+
+    }];
+}
+
+function saveCartToLocale() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 export function addToCart(productId, quantity) {
     let matchingItem;
     cart.forEach((cartItem) => {
@@ -22,6 +30,7 @@ export function addToCart(productId, quantity) {
             quantity
         })
     }
+    saveCartToLocale();
 }
 
 
@@ -35,5 +44,7 @@ export function deleteFromCart(productId) {
     })
 
     cart = newCart;
+    saveCartToLocale();
+
 
 }
